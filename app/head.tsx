ad24b7622628f38f2fd0195d116b93a3ebead19b
Noah Windham <noahwindham@Mac.lan>
2026-02-12 23:18:38 -0800

    Add Google verification tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="J2odBLATDAsa_8J1FJQFZHReovHTEZ3LxcfvUDLe1AM"
+      />
+    </>
+  );
+}
